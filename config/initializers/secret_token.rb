44b92367c5a7cf1fee7ba5a946b61437fe9b8b1a
KamilPludra <pludra@gmail.com>
2013-06-02 20:12:34 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WS::Application.config.secret_key_base = '4add7d7582593fa9d2e332db112259748d2a2cf8e3fab88e900e3bcefc67ace0c57941ebc2b9cdb94c4c022cd5b255e9d20ec541f325674b5c854e240df1ff27'
---
+++ config/routes.rb
@@ -0,0 +1,79 @@
+WS::Application.routes.draw do
+
+
+
+  get "password_resets/new"
+
+  resources :users do
+    member do
+      get :following, :followers
+
+    end
+  end
+
+
+  resources :sessions,   only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
+
+  resources :relationships, only: [:create, :destroy]
+
+  get '/zarejestruj',  to: 'users#new'
+  get '/zaloguj',  to: 'sessions#new'
+  #match '/wyloguj', to: 'sessions#destroy', via: :delete
+
+
+  root to: 'strony_statyczne#home'
+
+  get '/pomoc',    to: 'strony_statyczne#pomoc'
+  get '/onas',   to: 'strony_statyczne#onas'
+  get '/kontakt', to: 'strony_statyczne#kontakt'
+
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Example resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Example resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Example resource route with more complex sub-resources:
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', on: :collection
+  #     end
+  #   end
+
+  # Example resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+end

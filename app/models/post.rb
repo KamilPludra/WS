6dd92f16161a7ac26e1d1cad545d46b8b95eafb9
KamilPludra <pludra@gmail.com>
2013-06-08 01:21:38 +0200

    Improve the README

--- app/models/post.rb
+++
@@ -1,8 +0,0 @@
-def tag_list
-  self.tags.map { |t| t.name }.join(", ")
-end
-
-def tag_list=(new_value)
-  tag_names = new_value.split(/,\s+/)
-  self.tags = tag_names.map { |name| Tag.where('name = ?', name).first or Tag.create(:name => name) }
-end
